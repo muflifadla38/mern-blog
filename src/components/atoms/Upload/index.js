73a3429e960ce5427b3db435643e499066ca9a70
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-20 22:58:55 +0800

    Implementasi create post & upload image

--- src/components/atoms/Upload/index.js
+++ src/components/atoms/Upload/index.js
@@ -2,21 +2,21 @@ import PropTypes from "prop-types";
 import "./upload.scss";
 import Gap from "../Gap";
 
-const Upload = ({ label, src }) => {
+const Upload = ({ label, img, ...rest }) => {
   return (
     <div className="input-wrapper row">
       <div className="col-1">
         <p className="bold">{label}</p>
       </div>
       <div className="col">
-        <img src={src} className="preview" alt="PreviewImage" />
+        {img && <img src={img} className="preview" alt="PreviewImage" />}
         <Gap height={"0.5em"} />
-        <input type="file" accept="images/*" />
+        <input type="file" accept="images/*" {...rest} />
       </div>
     </div>
   );
 };
 
-Upload.propTypes = {label: PropTypes.string, src: PropTypes.string};
+Upload.propTypes = { label: PropTypes.string, img: PropTypes.string };
 
 export default Upload;
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./createblog.scss";
 import { Button, Gap, Input, TextArea, Upload } from "../../components";
-import { LoginBg } from "../../assets";
 import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 const CreateBlog = () => {
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const onUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    setImagePreview(URL.createObjectURL(file));
+  };
+
+  const onSubmit = () => {
+    console.log("title: ", title);
+    console.log("body: ", body);
+    console.log("image: ", image);
+    console.log("preview: ", imagePreview);
+
+    const data = new FormData();
+    data.append("title", title);
+    data.append("body", body);
+    data.append("image", image);
+
+    Axios.post("http://localhost:4000/v1/blog/create-post", data, {
+      Headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((res) => console.log("Post success: ", res))
+      .catch((err) => console.log("Err : ", err));
+  };
 
   return (
     <div className="create-blog-wrapper">
@@ -12,18 +43,33 @@ const CreateBlog = () => {
       <Gap height={"2em"} />
       <h3>Create New Blog Post</h3>
       <Gap height={"2em"} />
-      <Input label="Title" type="text" placeholder="Title" />
+      <Input
+        label="Title"
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <Gap height={"1em"} />
       <Input label="Author" type="text" placeholder="Author" />
       <Gap height={"1em"} />
-      <Upload src={LoginBg} label="Thumbnail" />
+      <Upload
+        img={imagePreview}
+        label="Thumbnail"
+        onChange={(e) => onUpload(e)}
+      />
       <Gap height={"1em"} />
-      <TextArea label="Body" placeholder="Post Body" />
+      <TextArea
+        label="Body"
+        placeholder="Post Body"
+        onChange={(e) => setBody(e.target.value)}
+      />
       <Gap height={"2em"} />
       <Button
         type="primary"
         label="Save"
-        onClick={() => navigate("/detail-blog")}
+        onClick={onSubmit}
+        // onClick={() => navigate("/detail-blog")}
       />
     </div>
   );
